d0d3261272d6d53bb61bc8793005a0c53a8b321c
Carlos Tapia <ctapiadev@gmail.com>
2026-01-07 20:41:09 -0300

    first commit

---
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.miprimeraapp;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.miprimeraapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private boolean esDisponible = true;
+    private int numeroProyectos = 15;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        inicializarEventos();
+    }
+
+    private void inicializarEventos() {
+        binding.btnContactar.setOnClickListener(v -> {
+            Toast.makeText(this, "Enviando correo...", Toast.LENGTH_LONG).show();
+
+            numeroProyectos++;
+            actualizarContadores();
+        });
+    }
+
+    private void actualizarContadores() {
+        binding.tvProyectosCount.setText(String.valueOf(numeroProyectos));
+    }
+}
